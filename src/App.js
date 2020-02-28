d4fb871323338c5fab39cadb445a81ceb6b64af1
jgam <jgam@alumni.nd.edu>
2020-02-28 14:54:20 +0900

    [mehdi] help fixing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React, { useRef, useState, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 import GetUserInfo from './GetUserInfo';
+import Auth from './Auth';
+import Button from '@rakuten-rex/button';
+import SignUpDesktop from '@rakuten-rex/sign-up-mobile';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom'
+
+require('dotenv').config()
 
 function countActiveUsers(users){
   console.log('counting the number of users...');
@@ -9,13 +20,45 @@ function countActiveUsers(users){
 }
 
 function App() {
+
+  /*
+  const data = {
+    'client_id': 'iot_pet_app',
+    'client_secret': 'dCkhfdFPgZd626ldN7oEv9p5XHG0r9lLEcR3Q-5qurL4',
+    'grant_type':'password',
+    'scope':'memberinfo_read_safebulk,memberinfo_read_details_safe,memberinfo_read_name,365days@Refresh,openid,pnp_ios_register,openid profile,idinfo_read_encrypted_easyid,pnp_android_register,30days@Access',
+    'username':'test-iot123@example.com',
+    'password':'123456789',
+
+  }
+  const axios = require('axios');
+
+// Make a request for a user with a given ID
+    axios.post('http://stg.app.rakuten.co.jp/engine/token?',
+      data)//client_id=iot_pet_app&client_secret=dCkhfdFPgZd626ldN7oEv9p5XHG0r9lLEcR3Q-5qurL4&grant_type=password&scope=memberinfo_read_safebulk,memberinfo_read_details_safe,memberinfo_read_name,365days@Refresh,openid,pnp_ios_register,openid profile,idinfo_read_encrypted_easyid,pnp_android_register,30days@Access&username=test-iot123@example.com&password=123456789')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        console.log('*****');
+        console.log(response.data.access_token);
+        const authValue = 'OAuth2 ' + response.data.access_token
+        //axios.post('http://stg.app.rakuten.co.jp/engine/api/MemberInformation/GetUserInfoSafe/20170926', {'Authorization': authValue})
+      })
+      .catch(function (error) {
+        // handle error
+        console.log('here is an error')
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
   const [inputs, setInputs] = useState({
     username: '',
     email: ''
   });
   const { username, email } = inputs;
   const onChange = useCallback(
-    e => {
+    e => {  
     const { name, value } = e.target;
     setInputs({
       ...inputs,
@@ -95,13 +138,33 @@ function App() {
     </>
   );
   */
-
+  function redirectStg(){
+    return fetch('https://stg.grp03.id.rakuten.co.jp/rms/nid/login?service_id=i103&client_id=iot_pet_app&redirect_uri=https%3A%2F%2Fstg-gcp.iot.mobile.rakuten.co.jp%2Flivepets%2Fauth&scope=memberinfo_read_safebulk%2Cmemberinfo_read_details_safe%2Cmemberinfo_read_name%2C365days%40Refresh%2Copenid%2Cpnp_ios_register%2Copenid&contact_info_required=false&rae_service_id=')
+  }
   return (
     <div>
       hello
+      <br></br>
+      <a href="https://stg.grp03.id.rakuten.co.jp/rms/nid/login?service_id=i103&client_id=iot_pet_app&redirect_uri=https%3A%2F%2Fstg-gcp.iot.mobile.rakuten.co.jp%2Flivepets%2Fauth&scope=memberinfo_read_safebulk%2Cmemberinfo_read_details_safe%2Cmemberinfo_read_name%2C365days%40Refresh%2Copenid%2Cpnp_ios_register%2Copenid&contact_info_required=false&rae_service_id=">go to Login</a>
       <GetUserInfo />
+      <Button onClick={redirectStg}>
+        Click me
+      </Button>
+      <Router>
+        <Link to="/auth">auth</Link>
+        <Switch>
+          <Route path="/auth">
+            <Auth />
+          </Route>
+        </Switch>
+      </Router>
     </div>
+
   )
+  
 }
+
+
+
 
 export default App;
--- src/GetUserInfo.js
+++ src/GetUserInfo.js
@@ -19,6 +19,35 @@ function GetUserInfo(){
         console.log(error);
       });
     */
+   const axios = require('axios');
+
+   axios.defaults.headers.common['Authorization'] = 'Bearer ("Access-Control-Allow-Origin", "*")';
+
+    
+// Make a request for a user with a given ID
+    axios.post('http://stg.app.rakuten.co.jp/engine/token?client_id=iot_pet_app&client_secret=dCkhfdFPgZd626ldN7oEv9p5XHG0r9lLEcR3Q-5qurL4&grant_type=password&scope=memberinfo_read_safebulk,memberinfo_read_details_safe,memberinfo_read_name,365days@Refresh,openid,pnp_ios_register,openid profile,idinfo_read_encrypted_easyid,pnp_android_register,30days@Access&username=test-iot123@example.com&password=123456789')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        console.log(response.data.access_token);
+        const authValue = 'OAuth2 ' + response.data.access_token
+        axios.get('http://stg.app.rakuten.co.jp/engine/api/MemberInformation/GetUserInfoSafe/20170926', {'headers':{'Authorization':authValue}}).then(
+          function(response){
+            console.log('the real response is: !!!!****')
+            console.log(response)
+          }
+        )
+
+      })
+      .catch(function (error) {
+        // handle error
+        console.log('here is an error')
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+    
     //render(){
         return (
             <div> getToken
