ef90fcc200206436ee3824a6337c14e93bd68ecb
jgam <jgam@alumni.nd.edu>
2020-03-16 13:52:39 +0900

    done with the logic

--- src/Userinfo.js
+++ src/Userinfo.js
@@ -3,9 +3,17 @@ import React from 'react'
 function Userinfo({infoUsers, handler}) {
     console.log('im in userinfo js');
     console.log(infoUsers)
+    console.log(infoUsers.openId);
+    console.log(infoUsers.emailAddress)
+
+    //here use the map key to display all the information
     return (
         <div>
             this is useinfo
+            Welcome {infoUsers.firstName}! Mr. {infoUsers.lastName}, I appreciate you so much today!
+
+            Your information is the following:
+            Email address : {infoUsers.emailAddress}
         </div>
     )
 }
